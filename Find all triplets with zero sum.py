ff4d87192915218cfb02f8c14423c23bf970a8ab
Elliot Mollman <129421289+ElliotMollman@users.noreply.github.com>
2024-04-12 19:24:06 -0700

    Add files via upload

---
+++ Find all triplets with zero sum.py
@@ -0,0 +1,38 @@
+#Coding question, Find all triplets in list with the sum of 0
+#Oroginal answer by Elliot Mollman
+
+def check_anwer(len_of_list, List):
+        start = 0
+
+
+        for number_1_position in range(start, len_of_list - 2):
+            number_1_value = List[number_1_position]
+
+            if List[number_1_position] == List[len_of_list - 2]:
+                break
+
+            for number_2_position in range(number_1_position, len_of_list-1):
+                number_2_value = (List[number_2_position + 1])
+
+                if List[number_2_position + 1] == List[len_of_list - 1]:
+                    break
+
+                for number_3_position in range(number_2_position, len_of_list):
+                    number_3_value = (List[number_3_position + 2])
+
+                    if number_1_value + number_2_value + number_3_value == 0:
+                        print(f"sum of {number_1_value}, {number_2_value}, and {number_3_value} = " + str(
+                            number_1_value + number_2_value + number_3_value))
+                    if List[number_3_position + 2] == List[len_of_list - 1]:
+                        print("finished loop")
+                        break
+
+
+
+
+
+
+List = [3, 6, -4, 1, -2, -1]
+len_of_list = len(List)
+
+check_anwer(len_of_list, List)
